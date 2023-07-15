7cf598c63ea107dc45b77e2661d02d60060881c1
asif10388 <asif.khan10388@gmail.com>
2023-07-15 17:31:43 +0600

    fixed next.config file

--- root.drio/next.config.js
+++ root.drio/next.config.js
@@ -4,13 +4,19 @@ const nextConfig = {
 
   env: {
     API_URL: process.env.API_URL,
+    MOCK_URL: process.env.MOCK_URL,
+    DEVELOPMENT_MODE: process.env.DEVELOPMENT_MODE,
   },
 
   async rewrites() {
     return [
       {
         source: "/api/:path*",
-        destination: `${process.env.API_URL}/:path*`,
+        destination: `${
+          process.env.DEVELOPMENT_MODE === "controller"
+            ? process.env.API_URL
+            : process.env.MOCK_URL
+        }/:path*`,
       },
     ];
   },
