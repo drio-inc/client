a5d3f60aba86265654155031f893ce20e02a0972
asif10388 <asif.khan10388@gmail.com>
2024-01-20 17:39:13 +0600

    Full page static loader

--- logistics-ui/src/comps/Sidebar/Sidebar.tsx
+++ logistics-ui/src/comps/Sidebar/Sidebar.tsx
@@ -40,7 +40,7 @@ export default function Sidebar() {
         <div
           className={`md:flex md:flex-col md:items-stretch md:opacity-100 md:relative md:mt-4 md:shadow-none shadow absolute top-0 left-0 right-0 overflow-y-auto overflow-x-hidden h-auto items-center flex-1 rounded`}
         >
-          <ul className="md:flex-col md:min-w-full flex flex-col list-none">
+          <ul className="md:flex-col md:min-w-full flex flex-col list-none gap-y-2">
             {NavLinks.map((link) => (
               <li key={link.name}>
                 <div
--- root.drio/src/comps/ui/Loader/StaticLoader.tsx
+++ root.drio/src/comps/ui/Loader/StaticLoader.tsx
@@ -3,8 +3,11 @@ import { RiLoader4Fill } from "react-icons/ri";
 
 const StaticLoader = () => {
   return (
-    <div className="flex justify-center items-center bg-transparent">
-      <RiLoader4Fill className="animate-spin text-5xl text-drio-red font-bold" />
+    <div className="fixed bg-transparent overflow-hidden flex justify-center items-center top-0 bottom-0 left-0 right-0 z-[1000]">
+      <RiLoader4Fill
+        className="animate-spin text-5xl text-drio-red font-bold"
+        data-testid="loading-svg"
+      />
     </div>
   );
 };
