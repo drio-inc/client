5c94fe637d2023f9830a8778cb08e915b4205076
asif10388 <asif.khan10388@gmail.com>
2024-01-22 16:21:38 +0600

    cleaned up stats popup

--- root.drio/src/comps/RootAdmin/Datasets/Metadata/SchemaStats/SchemaStats.tsx
+++ root.drio/src/comps/RootAdmin/Datasets/Metadata/SchemaStats/SchemaStats.tsx
@@ -1,59 +1,13 @@
 /* eslint-disable react/no-unescaped-entities */
-import Table from "@/comps/ui/Table";
-import Modal from "@/comps/ui/Modal";
-import Button from "@/comps/ui/Button";
 
-import {
-  setRows,
-  setRawRows,
-  setSelectedRows,
-} from "@/state/slices/metadataSlice";
-
+import { HiX } from "react-icons/hi";
 import { useEffect, useState } from "react";
-import { IoRefresh } from "react-icons/io5";
-import { HiMinusSm, HiPlus, HiX } from "react-icons/hi";
-import * as Checkbox from "@radix-ui/react-checkbox";
-import { setCloseModal, setOpenModal } from "@/state/slices/uiSlice";
+import { setCloseModal } from "@/state/slices/uiSlice";
 import { useAppDispatch, useAppSelector } from "@/hooks/useStoreTypes";
 
 import { useRouter } from "next/router";
-import getSchema from "@/functions/getSchema";
-import { mergedDDXData } from "@/functions/mergeDDXData";
-import { setRows as setDDXRows } from "@/state/slices/DDXSlice";
-import { mergedDataSourceData } from "@/functions/mergeDataSources";
-import { setRows as setDataSourceRows } from "@/state/slices/dataSourceSlice";
 import getSchemaStats from "@/functions/getSchemaStats";
 import { Fields } from "@/api/resources/datasets/types";
-
-type Params = {
-  id: string;
-  ou_id: string;
-  account_id: string;
-};
-
-const headers = [
-  {
-    header: "Dataset Name",
-    accessor: "property",
-  },
-  {
-    header: "Sample Value",
-    accessor: "sample_value",
-  },
-  {
-    header: "Data Type",
-    accessor: "property_type",
-  },
-  {
-    header: "Visibility",
-    accessor: "visibility",
-  },
-
-  {
-    header: "Last Updated",
-    accessor: "last_updated",
-  },
-];
 
 const SchemaStats = () => {
   const router = useRouter();
@@ -77,9 +31,7 @@ const SchemaStats = () => {
         ?.find((row) => row?.name === datasetName)
         ?.fields.find((row) => row?.metric === currentRow?.property);
 
-      if (data) {
-        setStats(data);
-      }
+      if (data) setStats(data);
     });
   }, [
     dispatch,
@@ -90,33 +42,33 @@ const SchemaStats = () => {
   ]);
 
   return (
-    <div className="w-full">
-      <div className={"flex flex-col w-full shadow-lg rounded-lg bg-white"}>
-        <span className="p-2 flex justify-end w-full">
-          <HiX
-            className="w-8 h-8 cursor-pointer hover:text-drio-red-dark transition-all duration-200"
-            onClick={() => dispatch(setCloseModal("schemStatsDetails"))}
-          />
-        </span>
-        <div className="mx-auto bg-white px-8 pb-8 rounded-lg">
-          <h2 className="text-gray-700 text-2xl font-bold text-center">
-            Stats for '{currentRow?.property}'
-          </h2>
+    <div className={"flex flex-col w-full shadow-lg rounded-lg bg-white"}>
+      <span className="p-2 flex justify-end w-full">
+        <HiX
+          className="w-8 h-8 cursor-pointer hover:text-drio-red-dark transition-all duration-200"
+          onClick={() => dispatch(setCloseModal("schemStatsDetails"))}
+        />
+      </span>
+      <div className="mx-auto bg-white px-8 pb-8 rounded-lg">
+        <h2 className="text-gray-700 text-2xl font-bold text-center">
+          Stats for '{currentRow?.property}'
+        </h2>
 
-          <div className="grid grid-cols-1 lg:grid-cols-2 gap-x-8 my-4 text-gray-700 text-sm gap-y-2 shadow-sm border rounded-md bg-indigo-50 p-4">
-            {stats ? (
-              Object.entries(stats).map(([key, value]) => (
-                <div className="flex gap-x-2" key={key}>
-                  <span className="capitalize font-medium">{key}:</span>
-                  <span>{value as string}</span>
-                </div>
-              ))
-            ) : (
-              <div className="flex justify-center w-full col-span-2">
-                <span className="capitalize font-medium">No data</span>
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-x-8 my-4 text-gray-700 text-sm gap-y-2 shadow-sm border rounded-md bg-indigo-50 p-4">
+          {stats ? (
+            Object.entries(stats).map(([key, value]) => (
+              <div className="flex gap-x-1" key={key}>
+                <span className="capitalize font-semibold">{key}:</span>
+                <span>{value as string}</span>
               </div>
-            )}
-          </div>
+            ))
+          ) : (
+            <div className="flex justify-center w-full col-span-2">
+              <span className="font-medium">
+                No stats found for this schema type
+              </span>
+            </div>
+          )}
         </div>
       </div>
     </div>
