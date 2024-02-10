670937bc7c554a0fe6ab7d5f588839d7192abc12
asif10388 <asif.khan10388@gmail.com>
2024-02-10 16:42:13 +0600

    docker test

--- root.drio/next.config.js
+++ root.drio/next.config.js
@@ -1,25 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
+  images: {
+    unoptimized: true,
+  },
+  output: "export",
   env: {
     API_URL: process.env.API_URL,
     MOCK_URL: process.env.MOCK_URL,
     DEVELOPMENT_MODE: process.env.DEVELOPMENT_MODE,
   },
-
-  // async rewrites() {
-  //   return [
-  //     {
-  //       source: "/api/:path*",
-  //       destination: `${
-  //         process.env.DEVELOPMENT_MODE === "mock"
-  //           ? process.env.MOCK_URL
-  //           : process.env.API_URL
-  //       }/:path*`,
-  //     },
-  //   ];
-  // },
 };
 
 module.exports = nextConfig;
