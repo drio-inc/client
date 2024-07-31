68d0f3427d889129be408632253a8916106dc389
asif10388 <asif.khan10388@gmail.com>
2024-07-31 20:16:07 +0600

    Fixed Rule Template disable/enable status

--- saas.drio/src/comps/SuperAdmin/RuleTemplates/RuleTemplatesMenu/RuleTemplatesMenu.tsx
+++ saas.drio/src/comps/SuperAdmin/RuleTemplates/RuleTemplatesMenu/RuleTemplatesMenu.tsx
@@ -1,3 +1,4 @@
+import { v4 as uuidv4 } from "uuid";
 import Modal from "@/comps/ui/Modal";
 import AlertModal from "@ui/AlertModal";
 import { HiDotsVertical } from "react-icons/hi";
@@ -7,6 +8,7 @@ import { useAppDispatch, useAppSelector } from "@/hooks/useStoreTypes";
 import { setOpenModal } from "@/state/slices/uiSlice";
 import UpdateRuleTemplateForm from "../UpdateRuleTemplateForm";
 import { setRows, setSelectedRows } from "@/state/slices/ruleTemplateSlice";
+import showAlert from "@/comps/ui/Alert/Alert";
 
 const RuleTemplatesMenu = ({ row }: TableRow) => {
   const dispatch = useAppDispatch();
@@ -15,6 +17,26 @@ const RuleTemplatesMenu = ({ row }: TableRow) => {
   const deleteRow = (id: string) => {
     dispatch(setRows(ruleTemplateState.rows.filter((row) => row.id !== id)));
     dispatch(setSelectedRows([]));
+  };
+
+  const handleToggle = () => {
+    dispatch(
+      setRows(
+        ruleTemplateState.rows.map((templateRow) =>
+          templateRow.id === row.id
+            ? { ...templateRow, enabled: templateRow.enabled === "yes" ? "no" : "yes" }
+            : templateRow
+        )
+      )
+    );
+
+    showAlert("Rule updated successfully", "success");
+  };
+
+  const handleClone = () => {
+    const findRow = ruleTemplateState.rows.find((templateRow) => templateRow.id === row.id);
+    dispatch(setRows([...ruleTemplateState.rows, { ...findRow, id: uuidv4() }]));
+    showAlert("Rule cloned successfully", "success");
   };
 
   return (
@@ -28,7 +50,7 @@ const RuleTemplatesMenu = ({ row }: TableRow) => {
           side="left"
           sideOffset={5}
           align="center"
-          className="bg-white rounded-lg shadow-lg text-sm text-gray-700"
+          className="bg-white rounded-lg shadow-lg text-sm text-gray-700 flex flex-col"
         >
           <span className={"cursor-pointer hover:bg-indigo-50 w-full block"}>
             <Modal
@@ -44,8 +66,18 @@ const RuleTemplatesMenu = ({ row }: TableRow) => {
             <AlertModal row={row} accessor={row.name} onClick={() => deleteRow(row.id)} />
           </span>
 
-          <Popover.Close className="cursor-pointer hover:bg-indigo-50 py-2 px-4 text-left">
-            Disable
+          <Popover.Close
+            onClick={handleToggle}
+            className="cursor-pointer hover:bg-indigo-50 py-2 px-4 text-left"
+          >
+            {row.enabled === "yes" ? "Disable" : "Enable"}
+          </Popover.Close>
+
+          <Popover.Close
+            onClick={handleClone}
+            className="cursor-pointer hover:bg-indigo-50 py-2 px-4 text-left"
+          >
+            Clone
           </Popover.Close>
         </Popover.Content>
       </Popover.Portal>
