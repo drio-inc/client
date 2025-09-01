9cc14656ca8d042e8e65b0d96087ecd6c32e6813
Asif Hossain Khan <asif.khan10388@gmail.com>
2025-09-02 01:20:19 +0600

    fix: Update API base URL to a hardcoded value in apiService

--- sc-events-generator/src/state/services/apiService.ts
+++ sc-events-generator/src/state/services/apiService.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const rootApi = createApi({
   reducerPath: "rootApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: `${process.env.API_URL || "https://controller.ddx.drio.ai:6443"}`,
+    baseUrl: `${"https://controller.ddx.drio.ai:6443"}`,
   }),
 
   endpoints: () => ({}),
